60a665de3297d577a74eb27644ddd552ee81eef8
USUARIO <USUARIO@192.168.0.18>
2017-10-29 21:28:44 -0300

    practicando para  la  prueba

---
+++ clasetarea2cecilia/aliciavaenelcaxe.java
@@ -0,0 +1,39 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+/**
+ *
+ * @author USUARIO
+ */
+public class aliciavaenelcaxe {
+    
+}
+      {
+      volumen = (3/4)*PI*(radio*radio*radio);
+      JOptionPane.showMessageDialog(null,"el volumen  de su  esfera es ");
+       }
+  }
+}  
+Impuestos
+
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package clase3a;
+import java.util.Scanner;
+/**
+ *
+ * @author LP 13
+ */
+public class Impuestos
+{
+  public Impuestos() //constructor
+  {
+  
+  }
+  public static void main (String[] args)
